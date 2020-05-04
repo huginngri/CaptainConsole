7dda0df4a763d38f5318562b8168f71a60e51a6a
kristjanari <kristjant18@ru.is>
2020-05-04 19:52:06 +0000

    smíða models

--- Captain_Console/products/models.py
+++ Captain_Console/products/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from manufacturers.models import Manufacturer
+from users.models import Customer
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=999)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    price = models.FloatField()
+    console_type = models.ForeignKey(ProductConsole, on_delete=models.CASCADE)
+    rating = models.FloatField()
+    def __str__(self):
+        return self.name
+
+class ProductConsole(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+
+class ProductImage(models.Model):
+    image = models.CharField(max_length=999)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    star = models.FloatField()
+    comment = models.CharField(max_length=999)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+class Search(models.Model):
+    search_content = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
---
+++ Captain_Console/users/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from products.models import Product
+
+# Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.CharField(max_length=999)
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
+    billing = models.ForeignKey(Billing, on_delete=models.CASCADE)
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
+    billing = models.ForeignKey(Billing, on_delete=models.CASCADE)
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+class Payment(models.Model):
+    card_holder = models.CharField(max_length=255)
+    card_number = models.CharField(max_length=255)
+    exp_date = models.CharField(max_length=255)
+    cvc = models.CharField(max_length=255)
+
+class Billing(models.Model):
+    full_name = models.CharField(max_length=255)
+    street_name = models.CharField(max_length=255)
+    house_number = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    zip = models.CharField(max_length=255)
