c82406eae45008c6e34673905e1afe665bea27d6
Þóranna Dís Bender <thoranna18@ru.is>
2020-05-05 18:08:19 +0000

    semi ready searh

--- Captain_Console/products/views.py
+++ Captain_Console/products/views.py
@@ -2,8 +2,22 @@ from django.shortcuts import render
 from products.models import Product
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 
 def index(request):
+
+    if 'search_filter' in request.GET:
+        search_filter = request.GET('search-filter')
+        products = [{
+            'name': x.name,
+            'description': x.name,
+            'price': x.price,
+            'rating': x.rating
+            # 'console-type-id'
+            # 'manufacturer-id'
+        } for x in Product.objects.filter(name__icontains=search_filter)]
+        return JsonResponse({'data': products})
+
     context = {'products': Product.objects.all().order_by('name')}
     return render(request, 'products/index.html', context)
 
--- Captain_Console/static/js/products.js
+++ Captain_Console/static/js/products.js
@@ -2,13 +2,17 @@ $(document).ready(function() {
     $('#search-btn').on('click', function(e) {
         e.preventDefault();
         var searchText = $('#search-box').val();
+        console.log(searchText)
         $.ajax({
-            url: '/products' + searchText,
+            url: '/products?search_filter=' + searchText,
             type: 'GET',
             success: function(resp) {
+                var newHtml = resp.data
                 console.log('success')
+                console.log(searchText)
             },
             error: function(xhr, status, error) {
+                console.log('error')
                 console.error(error)
             }
         })
