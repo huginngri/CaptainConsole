2c69c2e19ef00a3a11f9ebfc12d4221e4c6b7578
kristjanari <kristjant18@ru.is>
2020-05-06 16:57:50 +0000

    nýr branch

--- Captain_Console/consoles/urls.py
+++ Captain_Console/consoles/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from . import views
+from . import views as v1
+from products import views as v2
+
+
 
 urlpatterns = [
     #http://localhost:8000/manufacturers/:manufacturerName/consoles/
-    path('', views.index, name="consoles-index"),
-    path('<str:name>', views.get_console_by_name, name="consoles-product"),
+    path('', v1.index, name="consoles-index"),
+    path('<str:name>', v1.get_console_by_name, name="consoles-product"),
+    path('<str:name>/consoles/', v1.get_consoles_by_name_console_names, name="consoles-consoles"),
+    path('<str:name>/consoles/<int:id>', v2.get_product_by_id, name="consoles-one-console"),
+    path('<str:name>/games/', v1.get_games_by_name_console_names, name="consoles-games"),
+    path('<str:name>/games/<int:id>', v2.get_product_by_id, name="consoles-one-game"),
+    path('<str:name>/accessories/', v1.get_accessories_by_name, name="consoles-accessories"),
+    path('<str:name>/accessories/<int:id>', v2.get_product_by_id, name="consoles-one-accessory")
 ]
--- Captain_Console/consoles/views.py
+++ Captain_Console/consoles/views.py
@@ -10,5 +10,20 @@ def index(request):
 
 def get_console_by_name(request, name):
     console = Console.objects.get(name=name)
-    context = {'console': console, 'products': Product.objects.filter(console_type=console.id)}
+    context = {'console': console, 'products': Product.objects.filter(console_type=console.id), 'filter': 'none'}
+    return render(request, 'consoles/console_details.html', context)
+
+def get_consoles_by_name_console_names(request, name):
+    console = Console.objects.get(name=name)
+    context = {'console': console, 'products': Product.objects.filter(console_type=console.id, type='console'), 'filter': 'console'}
+    return render(request, 'consoles/console_details.html', context)
+
+def get_games_by_name_console_names(request, name):
+    console = Console.objects.get(name=name)
+    context = {'console': console, 'products': Product.objects.filter(console_type=console.id, type='game'), 'filter': 'games'}
+    return render(request, 'consoles/console_details.html', context)
+
+def get_accessories_by_name(request, name):
+    console = Console.objects.get(name=name)
+    context = {'console': console, 'products': Product.objects.filter(console_type=console.id, type='accessory'), 'filter': 'accessories'}
     return render(request, 'consoles/console_details.html', context)
