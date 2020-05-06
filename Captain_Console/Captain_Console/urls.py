70b3fe064c12f5337895fd228cb3711a5236e801
Flatkaka <hilmarpall@hotmail.com>
2020-05-06 19:48:45 +0000

    kristjan er brjalaedingur

--- Captain_Console/Captain_Console/urls.py
+++ Captain_Console/Captain_Console/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('products.urls')),
     path('', include('products.urls2')),
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
