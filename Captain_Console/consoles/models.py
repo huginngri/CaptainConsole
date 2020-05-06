ab5eb3f5fcade3f8a57a34d07c250fec96d70fa4
huginngri <huginngri@gmail.com>
2020-05-06 19:53:39 +0000

    Laga db

--- Captain_Console/consoles/models.py
+++ Captain_Console/consoles/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+from manufacturers.models import Manufacturer
 
 # Create your models here.
 
 class Console(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
--- Captain_Console/population_script.sql
+++ Captain_Console/population_script.sql
@@ -3,11 +3,7 @@ INSERT INTO products_productconsole (name) VALUES('Playstation 1');
 INSERT INTO products_product (name, description, price, rating, console_type_id, manufacturer_id) VALUES ('MGS1', 'The game mgs', 10.1, 4.5, 1, 1)
 INSERT INTO products_productimage (image, product_id) VALUES ('https://romsemulator.net/wp-content/uploads/mnt2/images/2018/07/5139front-61287-2.jpg', 1)
 INSERT INTO products_productimage (image, product_id) VALUES ('https://cdn.mos.cms.futurecdn.net/nHysFiFzH3k9jfV6VCcgb.jpg', 1)
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 9e5a64e7bd6daa6b2ea207bba0c19795f4db5159
 INSERT INTO products_productimage (image, product_id) VALUES ('images/captain-console.jpg', 1)
 INSERT INTO manufacturer_manufacturer (name, description, logo) VALUES('Playstation', 'The best console', 'images/captain-console');
 
@@ -46,11 +42,11 @@ INSERT INTO products_productconsole (name) VALUES('Atari2600');
 
 INSERT INTO products_product (name, description, price, rating, console_type_id, manufacturer_id) VALUES ('Atari 2600 console', 'A lovely console', 20.99, 4.5, 6, 6);
 INSERT INTO products_productimage (image, product_id) VALUES ('images/atari2600.jpg', 12);
-
+DELETE FROM django_migrations where id =58
 TRUNCATE Table products_product CASCADE ;
 TRUNCATE Table consoles_console CASCADE ;
 TRUNCATE Table manufacturers_manufacturer CASCADE ;
-TRUNCATE Table products_productimage CASCADE ;
+TRUNCATE Table django_migrations CASCADE ;
 
 
 CREATE TABLE products_productconsole (
