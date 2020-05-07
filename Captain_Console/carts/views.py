24ba3d9c669d4c2c6255edd85393e63aaf141a57
kristjanari <kristjant18@ru.is>
2020-05-07 13:36:49 +0000

    huginn  pulla

--- Captain_Console/carts/views.py
+++ Captain_Console/carts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from users.models import Customer
 from carts.models import Cart
+from carts.models import CartDetails
 
 # Create your views here.
-def add_to_cart(request, product_id):
+def add_to_cart(request, product):
     customer = Customer.objects.filter(user=request.user).first()
     cart = Cart.objects.filter(user=customer.id)
+    cart_detail = CartDetails(cart = cart, product=product)
+    cart_detail.save()
