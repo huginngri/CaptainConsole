94ad7ea002b147231be00d2bef7045504ff8f1cb
huginngri <huginngri@gmail.com>
2020-05-08 11:57:36 +0000

    Flott

--- Captain_Console/carts/views.py
+++ Captain_Console/carts/views.py
@@ -17,13 +17,17 @@ def add_to_cart(request):
         cart_detail = CartDetails(cart = cart, product=product)
         cart_detail.save()
         return JsonResponse({'count': len(CartDetails.objects.filter(cart=cart.id))})
-    else:
-        return JsonResponse({'error': 'Request failed'})
-
-def count_cart(request, id):
-    if request.method == 'GET':
+    elif request.method == "GET":
         customer = Customer.objects.filter(user=request.user).first()
         cart = Cart.objects.filter(user=customer.id).first()
         return JsonResponse({'count': len(CartDetails.objects.filter(cart=cart.id))})
     else:
         return JsonResponse({'error': 'Request failed'})
+
+#def count_cart(request, id):
+ #   if request.method == 'GET':
+  #      customer = Customer.objects.filter(user=request.user).first()
+   #     cart = Cart.objects.filter(user=customer.id).first()
+    #    return JsonResponse({'count': len(CartDetails.objects.filter(cart=cart.id))})
+    #else:
+     #   return JsonResponse({'error': 'Request failed'})
---
+++ Captain_Console/users/forms/user_form.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.forms import ModelForm, widgets
+
+
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        exclude = ['id', 'password', 'last_login', 'is_superuser', 'is_staff', 'is_active', 'date_joined']
+        widgets = {
+            'username': widgets.TextInput(attrs={'class': 'form-control'}),
+            'first_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'last_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'email': widgets.TextInput(attrs={'class': 'form-control'})
+        }
