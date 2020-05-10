2e6c92bab50a1209e377816e93c0644074342955
kristjanari <kristjant18@ru.is>
2020-05-10 20:50:29 +0000

    pull master

--- Captain_Console/orders/urls.py
+++ Captain_Console/orders/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
 
     #http://localhost:8000/carts
     path('checkout/billing', views.checkout_billing, name="checkout-billing"),
-    path('checkout/payment', views.checkout_payment, name="checkout-payment")
+    path('checkout/payment', views.checkout_payment, name="checkout-payment"),
+    path('checkout/savebilling', views.save_billing, name="checkout-save-billing")
 
 ]
--- Captain_Console/orders/views.py
+++ Captain_Console/orders/views.py
@@ -17,14 +17,27 @@ def checkout_billing(request):
         form = BillingForm(instance=profile.billing, data=request.POST)
         if form.is_valid():
             new_billing = form.save()
-            profile.billing = new_billing
-            profile.save()
-            return checkout_payment(request, new_billing.id)
+            #profile.billing = new_billing
+            #profile.save()
+            return redirect('orders/payment_checkout.html', {'billing_id':  new_billing.id})
     return render(request, "orders/billing_checkout.html", {
         "form": BillingForm(instance=profile.billing)
     })
 
-def checkout_payment(request, billing_id):
+def save_billing(request):
+    profile = Customer.objects.filter(user=request.user).first()
+    if request.method == "POST":
+        form = BillingForm(instance=profile.billing, data=request.POST)
+        if form.is_valid():
+            new_billing = form.save()
+            profile.billing = new_billing
+            profile.save()
+            return redirect('checkout-billing')
+    return render(request, "orders/billing_checkout.html", {
+        "form": BillingForm(instance=profile.billing)
+    })
+
+def checkout_payment(request):
     profile = Customer.objects.filter(user=request.user).first()
     if request.method == "POST":
         form = PaymentForm(data=request.POST)
@@ -32,7 +45,7 @@ def checkout_payment(request, billing_id):
             new_payment = form.save()
             profile.payment = new_payment
             profile.save()
-            return create_order(request, billing_id, new_payment.id)
+            return create_order(request, request.POST['billing_id'], new_payment.id)
     return render(request, "orders/payment_checkout.html", {
         "form": PaymentForm()
     })
