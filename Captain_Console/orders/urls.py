1d74a77ef1a3fb52f8459e29467e07dfa61e2a47
kristjanari <kristjant18@ru.is>
2020-05-11 13:03:03 +0000

    hilmar pull

--- Captain_Console/orders/urls.py
+++ Captain_Console/orders/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     #http://localhost:8000/carts
     path('checkout', views.checkout, name="checkout"),
     path('checkout/savebilling', views.save_billing, name="checkout-save-billing"),
-    path('checkout/savepayment', views.save_payment, name="checkout-save-payment")
+    path('checkout/savepayment', views.save_payment, name="checkout-save-payment"),
+    path('checkout/overview', views.create_order, name="checkout-overview")
 ]
