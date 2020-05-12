e5ad982fab80c0b2735e6fc5ec5e55344bc35364
Þóranna Dís Bender <thoranna18@ru.is>
2020-05-12 13:55:39 +0000

    order details

--- Captain_Console/orders/views.py
+++ Captain_Console/orders/views.py
@@ -128,14 +128,23 @@ def order_history(request):
     order = Order.objects.filter(customer=profile.id).first()
     order_details = OrderProduct.objects.filter(order=order)
     total = 0
+    no_of_orders = 0
 
     for order in orders:
         order_details = OrderProduct.objects.filter(order=order)
         for order_detail in order_details:
             product = Product.objects.get(id=order_detail.product_id)
             total += product.price
-        order.total = total
+        order.total = str(round(total, 2)) + " $"
+        order.address = order.billing.street_name + " " + order.billing.house_number + ", " + order.billing.zip + ", " + order.billing.country
         total = 0
+        if order.confirmed == True:
+            order.status = "Done"
+        else:
+            order.status = "Open"
+        no_of_orders += 1
+
+    orders.no = no_of_orders
 
     context = {'orders': orders}
     return render(request, "orders/order_history.html", context)
