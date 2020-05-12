bceccd318025b9a13450e20e905a317111c5df1d
kristjanari <kristjant18@ru.is>
2020-05-12 11:16:41 +0000

    sry, var ovart að vinna á master, er að gera remove from cart

--- Captain_Console/carts/views.py
+++ Captain_Console/carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from users.models import Customer
 from carts.models import Cart
 from carts.models import CartDetails
+from orders.models import Order, OrderProduct
 from products.models import Product
 from django.http import JsonResponse
 from users.forms.payment_form import PaymentForm
@@ -34,10 +35,26 @@ def view_cart(request):
     cart_details = CartDetails.objects.filter(cart=cart)
     products = []
     total = 0
-
     for cart_detail in cart_details:
         product = Product.objects.filter(id=cart_detail.product.id).first()
         products.append(product)
         total += product.price
+    orders = Order.objects.filter(customer=customer)
+    for order in orders:
+        if order.confirmed == False:
+            order_products = OrderProduct.objects.filter(order=order)
+            for order_product in order_products:
+                order_product.delete()
+            order.delete()
     context = {'cart': cart, 'products': products, 'total_price': total}
     return render(request, 'carts/cart_details.html', context)
+
+def remove_from_cart(request, product_id):
+    customer = Customer.objects.filter(user=request.user).first()
+    if request.method == 'DELETE':
+        cart = Cart.objects.filter(user=customer.id).first()
+        product = Product.objects.get(id=product_id)
+        cart_detail = CartDetails.objects.filter(cart=cart, product=product).first()
+        cart_detail.delete()
+        return JsonResponse({'message': 'Product removed from cart'})
+    return JsonResponse({'message': 'invalid request'})
