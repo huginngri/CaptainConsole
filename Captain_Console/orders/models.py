2c8375e98514d6a20e29fc24205e9dbd453a9889
Flatkaka <hilmarpall98@hotmail.com>
2020-05-13 16:19:09 +0000

    laga view all products.

--- Captain_Console/orders/models.py
+++ Captain_Console/orders/models.py
@@ -6,18 +6,18 @@ from products.models import Product
 # Create your models here.
 
 class Payment(models.Model):
-    card_holder = models.CharField(max_length=255)
-    card_number = models.CharField(max_length=255)
-    exp_date = models.CharField(max_length=255)
-    cvc = models.CharField(max_length=255)
+    card_holder = models.CharField(max_length=255,null=True, blank=True)
+    card_number = models.CharField(max_length=255,null=True, blank=True)
+    exp_date = models.CharField(max_length=255,null=True, blank=True)
+    cvc = models.CharField(max_length=255,null=True, blank=True)
 
 class Billing(models.Model):
-    full_name = models.CharField(max_length=255)
-    street_name = models.CharField(max_length=255)
-    house_number = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    country = models.CharField(max_length=255)
-    zip = models.CharField(max_length=255)
+    full_name = models.CharField(max_length=255,null=True, blank=True)
+    street_name = models.CharField(max_length=255,null=True, blank=True)
+    house_number = models.CharField(max_length=255,null=True, blank=True)
+    city = models.CharField(max_length=255,null=True, blank=True)
+    country = models.CharField(max_length=255,null=True, blank=True)
+    zip = models.CharField(max_length=255,null=True, blank=True)
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
