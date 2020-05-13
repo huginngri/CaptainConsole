482d39ca88f293dc9e5368ba8d66916fe117a370
Flatkaka <hilmarpall98@hotmail.com>
2020-05-13 14:33:44 +0000

    laga

--- Captain_Console/static/js/carts.js
+++ Captain_Console/static/js/carts.js
@@ -223,26 +223,27 @@ function closeDiv(orderNumber) {
 }
 
 
-function calculateRating(id) {
-    console.log("what");
-    star_div = document.getElementById("star_" + id);
-    rating = star_div.name
-    let star = document.createElement("span");
-    star.setAttribute("class", "glyphicon glyphicon-star");
-    let empty_star = document.createElement("span");
-    empty_star.setAttribute("class", "glyphicon glyphicon-star-empty");
-     // <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
-    let total_rating = 5;
-    while (rating > 0){
-        star_div.appendChild(star);
-        total_rating -= 1;
-        rating -= 1;
-    }
-    while(total_rating > 0){
-        star_div.appendChild(empty_star);
-        total_rating -= 1;
+function calculateRating(id, star) {
 
-    }
+        star_div = document.getElementById("star_" + id);
+        let rating = star_div.children[0].children[2];
+ //       let star = document.createElement("span");
+        star.setAttribute("class", "glyphicon glyphicon-star");
+        let empty_star = document.createElement("span");
+        empty_star.setAttribute("class", "glyphicon glyphicon-star-empty");
+        // <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
+        let total_rating = 5;
+        while (rating > 0) {
+            star_div.appendChild(star);
+            total_rating -= 1;
+            rating -= 1;
+        }
+        while (total_rating > 0) {
+            star_div.appendChild(empty_star);
+            total_rating -= 1;
+
+        }
+
 }
 
 function closeErrorDiv() {
