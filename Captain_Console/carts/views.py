f359880f8c79c76ccb7c00430b8c093fa97387d4
Þóranna Dís Bender <thoranna18@ru.is>
2020-05-14 16:16:38 +0000

    breyta

--- Captain_Console/carts/views.py
+++ Captain_Console/carts/views.py
@@ -69,6 +69,7 @@ def view_cart(request):
             order.delete()
     context1 = {'profile': customer, 'products': products, 'total_price': total}
     context2 = {'profile': customer}
+    print(customer.user)
 
     if total_in_cart != 0:
         return render(request, 'carts/cart_details.html', context1)
